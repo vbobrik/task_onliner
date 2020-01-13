ab3d2b0530160ebe16582924cc63ed1549536932
Valiantsina Bobryk <valiantsina.bobryk@itechart-group.com>
2020-01-13 18:54:59 +0300

    case1 without assert, half of 2

--- index.js
+++ index.js
@@ -10,15 +10,32 @@ const SEARCH_WORD = 'catalog';
     try{
 const driver = await new Builder()
 .forBrowser('chrome')
-// .setChromeOptions(new chrome.Options().windowSize(1080, 1080))
+// установить нужное разрешение окна (> 1000px)
 .setChromeOptions(new chrome.Options().addArguments(`window-size=${width},${height}`))
 .build();
-// установить нужное разрешение окна (> 1000px)
+
 await driver.get(URL);
 await console.log('------------url here');
+//draft
 // await driver.findElement(By.css('a[href^="https://catalog.onliner.by"]')).click();
-await driver.findElement(By.partialLinkText("https://catalog.onliner.by")).click();
-await (await driver.findElement(By.css('a[href^="https://catalog.onliner.by/mobile"]'))).click();
+// await driver.findElement(By.partialLinkText("https://catalog.onliner.by")).click();
+// await (await driver.findElement(By.css('a[href^="https://catalog.onliner.by/mobile"]'))).click();
+// await driver.findElement(By.css('.schema-product.schema-product_narrow-sizes'));
+
+//1st case
+// await driver.findElement(By.xpath('.//div/nav//*[text()="Каталог"]')).click();
+// await driver.findElement(By.xpath('.//*[text()="Мобильные телефоны"]')).click();
+// await driver.executeScript('window.scrollBy(0,300)');
+// await (await driver.findElement(By.xpath('.//li//*[text()="HONOR"]'))).click();
+// await driver.findElement(By.css('.schema-order__link')).click();
+// await driver.findElement(By.xpath('.//*[text()="Дорогие"]')).click();
+// GOOD
+
+
+//2nd case
+await driver.findElement(By.css('.auth-bar__item.auth-bar__item--text')).click();
+await driver.findElement(By.css("[href$='registration']")).click(); //'Зарегистрироваться'
+
 }
 catch(err) {
     console.error(err);
---
+++ pages/locators.js
@@ -0,0 +1,5 @@
+const MAIN_URL = 'https://www.onliner.by/';
+const CATALOG_BUTTON = By.xpath('.//div/nav//*[text()="Каталог"]');
+const MOBILE_PHONES_BUTTON = By.xpath('.//*[text()="Мобильные телефоны"]');
+const HONOR_CHECKBOX = By.xpath('.//li//*[text()="HONOR"]');
+const EXPENSIVE_DROPDOWN_VALUE = By.xpath('.//*[text()="Дорогие"]');
